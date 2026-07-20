02c428883574f3b36b50e17e40974f99f20523dd
Aaryaps003 <aaryaps003@gmail.com>
2026-07-20 14:02:15 +0530

    DBSQL Initiated

--- netflix/app/views.py
+++ netflix/app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .movies import movies_data
+from .models import Movie
 # Create your views here.
 def home(request):
 
 
-    movies = movies_data
+    movies = Movie.objects.all()
     return render(request, 'home.html', {'movies':movies})
