c63925cb3eda84b326e61152bbfbfb5a5f1c60e6
Aaryaps003 <aaryaps003@gmail.com>
2026-07-21 13:59:06 +0530

    Final Deployment Done

--- netflix/app/models.py
+++ netflix/app/models.py
@@ -4,7 +4,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     releaseYear = models.IntegerField()
-    rating = models.CharField(max_length=100)
+    rating = models.CharField(max_length=20)
     duration = models.CharField(max_length=20)
     director = models.CharField(max_length=100)
     cast = models.TextField()
--- netflix/app/views.py
+++ netflix/app/views.py
@@ -1,8 +1,14 @@
+from multiprocessing import context
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from .movies import movies_data
 from .models import Movie
 # Create your views here.
 def home(request):
 
 
     movies = Movie.objects.all()
-    return render(request, 'home.html', {'movies':movies})
+    print(movies)
+    context = {'movies': movies}
+    return render(request, 'home.html', context)
